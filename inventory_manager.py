1f8d3aef84bcc1cdc988c13307f2a0d9763aab71
R-Z-W <83737979+R-Z-W@users.noreply.github.com>
2023-12-21 22:48:01 +1100

    Add ability to add new product to database and fix case insensitvity
    New products that are detected are added to the end of the database.
    Case insensivity fixed to allow for better detection of exisiting products

--- inventory_manager.py
+++ inventory_manager.py
@@ -2,9 +2,9 @@
 TODO
 Input File
 	Input Daily Log of Products COMPLETE
-		Create Daily Log Template 
+		Create Daily Log Template COMPLETE
 	Input Product Database COMPLETE
-		Fix Product Database 
+		Fix Product Database COMPLETE
 
 Data Manipulation
 	Calculate Product Usage (used minus total stock)
@@ -21,8 +21,8 @@ Data Manipulation
 	Find Cheaper Product
 
 Database
-	Comb over database and check if product exist
-	Add To Database if doesnt exist
+	Comb over database and check if product exist COMPLETE
+	Add To Database if doesnt exist INPROGRESS
 	Track History of product use and Store in database
 	Search Database for products
 	Show information such as ratio, cost, etc.
@@ -47,23 +47,40 @@ Possible Additions
 
 
 import pandas as pd
-import numpy as np
+#import numpy as np
 
-
-# daily_log = open('log.txt', 'r')  input("Input Daily Log File Here: ")
-# print(daily_log.readlines())
 
 def find_product():
     csv_f = 'detailing_database.csv' #input("Input Database File Here: ")
     csv_database = pd.read_csv(csv_f)
     csv_name = list(csv_database['Name'])
+    csv_case_name = list((item.casefold() for item in csv_name)) #Case Insensitive
+    print(f'case: {csv_case_name}')
     for key in daily_log_dic.keys():
-        if key in csv_name:
-            print(f" {key}: Already Exists" )
-            print(int(csv_name.index(key)) + 2)
-            csv_database.loc[csv_name.index(key),'InUse'] = 'TRUE'
+        if key.startswith('Recorded'):
+            print("Recorded")
+        elif key.casefold() in csv_case_name:
+            # Change to tr
+            print(f"{key}: Already Exists" )
+            # print(int(csv_name.index(key)) + 2)
+            csv_database.loc[csv_name.index(key),'InUse'] = True
             csv_database.to_csv(csv_f, index=False)
+        else:
+            # Add new product to database file
+            print(f'\nNew Product Found: {key}\n ')
+            yes_no = input('-Add To Database? Y/N: ')
+            if yes_no == 'Y' or 'y' or 'Yes' or 'yes':
+                csv_category = set(csv_database['Category'])
+                print(f'Exisiting Categories: {csv_category}')
+                category = input('-Input Category: ')
+                overall_price =  int(input("-Input Price: "))
+                quantity =  int(input("-Input Quantity: "))
+                unit_price = overall_price / quantity
+                new_row = {'Name': key,'Category': category,'Overall Price': overall_price,'Quantity': quantity,'InUse': True,'Unit Price': unit_price, 'Ratio': input('-Input Ratio: ')}
+                csv_database = csv_database.append(new_row, ignore_index=True)
+                print(csv_database.tail(1))
                 
+
 
 daily_log_dic = {}
 
@@ -78,7 +95,7 @@ with open('log1.txt', "r") as daily_log: #input("Input Daily Log File Here: ")
             product_key_value = tuple(line.strip().split(':'))
             products.append(product_key_value)
             #daily_log_dic = dict(product_data)
-    daily_log_dic= dict(products)
+    daily_log_dic = dict(products)
 # print(daily_log_dic)
 
 find_product()
